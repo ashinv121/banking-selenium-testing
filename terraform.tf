eb7a7bd90b9303b1b9f0087517840cb7fc19080e
Ashin v <37022429+ashinv121@users.noreply.github.com>
2023-08-28 00:18:26 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "selenium_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 }
 
 resource "aws_security_group" "test_server_group" {
@@ -60,6 +67,13 @@ resource "aws_security_group" "test_server_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 }
 
 # Create EC2 instances
